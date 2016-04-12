00f629eb8e3cd07d0e2bf40e433af4ce3ce2b941
reachkvperumal <indianakv@gmail.com>
2016-04-11 21:19:26 -0400

    Initial commit

---
+++ src/main/java/com/ocp/chapter1/Tutorial.java
@@ -0,0 +1,11 @@
+package com.ocp.chapter1;
+
+/**
+ * Created by KV on 4/11/2016.
+ */
+public class Tutorial {
+
+    public static void main(String[] args){
+        System.out.println("HELLO WORLD...");
+    }
+}
