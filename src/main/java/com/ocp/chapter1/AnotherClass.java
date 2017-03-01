6e5ae3f373f64146c3ab3849a0f600faa768035a
reachkvperumal <indianakv@gmail.com>
2017-02-28 22:05:45 -0500

    Modifier definitions

--- src/main/java/com/ocp/chapter1/AnotherClass.java
+++ src/main/java/com/ocp/chapter1/AnotherClass.java
@@ -5,9 +5,13 @@ package com.ocp.chapter1;
  */
 public class AnotherClass {
 
+    protected boolean hasFur = true;
+
     public static void main(String[] args) {
         Tutorial obj = new Tutorial();
         System.out.println(obj.bestCaptain);
         System.out.println(obj.favoriteCaptian);
+        AnotherClass anotherClass = new AnotherClass();
+        System.out.println(anotherClass.hasFur);
     }
 }
---
+++ src/main/java/com/office/AClass.java
@@ -0,0 +1,14 @@
+package com.office;
+
+import com.ocp.chapter1.AnotherClass;
+
+/**
+ * Created by KV on 2/28/2017.
+ */
+public class AClass extends AnotherClass {
+
+    public static void main(String... args) {
+        AClass aClass = new AClass();
+        System.out.println(aClass.hasFur);
+    }
+}
