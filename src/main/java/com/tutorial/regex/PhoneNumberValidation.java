7f171f8b798547ed069c514ff3dc21e2ff4a2c15
reachkvperumal <indianakv@gmail.com>
2017-03-29 23:21:03 -0400

    Updated regex.

--- src/main/java/com/tutorial/regex/PhoneNumberValidation.java
+++ src/main/java/com/tutorial/regex/PhoneNumberValidation.java
@@ -10,17 +10,16 @@ public class PhoneNumberValidation {
 
     static void validate() {
         // String[] phone = {"2014567890","2004567890","9984567990", "", "20145678901", "2014", "1234567890", "20145678902014567802014567890"};
-        String[] phone = {""};
-        String regex = "^((2[0-9][0-9]|3[0-9][0-9]|4[0-9][0-9]|5[0-9][0-9]|6[0-9][0-9]|7[0-9][0-9]|8[0-9][0-9]|9[0-9][0-8])[0-9][0-9][0-9][0-9][0-9][0-9][0-9]){1}$";
+        String[] phone = {"2123433712"};
+        String regex = "^(((2[0-9][0-1]|2[0-9][3-9]|2[0-1][0-9]|2[3-9][0-9]|3[0-9][0-9]|4[0-9][0-9]|5[0-9][0-9]|6[0-9][0-9]|7[0-9][0-9]|8[0-9][0-9]|9[0-9][0-8]))" +
+                "[0-9][0-9][0-9][0-9][0-9][0-9][0-9])$";
         Stream.of(phone).forEach(t -> System.out.println(Pattern.compile(regex).matcher(t).matches()));
 
+        String regxp1 = "(?i)(?:([\\d])(?!\\1+))*";
+        System.out.println(Pattern.compile(regxp1).matcher("515").matches());
     }
 
     public static void main(String[] args) {
         validate();
-
-        int[] counter = new int[1];
-        System.out.println(counter[0]);
-        System.out.println('\u0000');
     }
 }
