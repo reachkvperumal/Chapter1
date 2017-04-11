37f8f18ebc89aeb162c5b89707cbac2845679a42
reachkvperumal <indianakv@gmail.com>
2017-04-10 23:34:12 -0400

    Updated regex.

--- src/main/java/com/tutorial/streams/LeaningMap.java
+++ src/main/java/com/tutorial/streams/LeaningMap.java
@@ -1,18 +1,30 @@
 package com.tutorial.streams;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * Created by KV on 4/6/2017.
  */
 public class LeaningMap {
 
-    static void init() {
+    public static void init() {
         Stream<Locale> locales = Stream.of(Locale.getAvailableLocales());
 
+        /*Map<String, String> stringMap = locales.collect(
+                Collectors.toMap(Locale::getDisplayLanguage, l -> l.getDisplayLanguage(l), (o, e) -> e)
+        );
+
+        System.out.println("----------------");
+        System.out.println(stringMap);
+        System.out.println("----------------");*/
         Map<String, String> map = locales.collect(
                 Collectors.toMap(l -> l.getDisplayLanguage(),
                         l -> l.getDisplayLanguage(l),
@@ -24,7 +36,13 @@ public class LeaningMap {
     }
 
     public static void main(String[] args) {
-        init();
+
+        //init();
+        Stream<Locale> stream = Arrays.stream(Locale.getAvailableLocales());
+        Map<String, List<Locale>> listMap = stream.collect(groupingBy(Locale::getCountry));
+        System.out.println(listMap);
+        Arrays.stream(Locale.getAvailableLocales()).collect(groupingBy(Locale::getCountry, counting()))
+                .forEach((s, aLong) -> System.out.printf("COUNTRY NAME : %s, COUNT = %d%n", new Locale("", s).getDisplayCountry(), aLong));
     }
 
 }
