fb34a31e45f8673d8e8ef6b2d6a5cf004798631e
reachkvperumal <indianakv@gmail.com>
2018-03-05 23:26:54 -0500

    Added source code

--- src/main/java/com/ocp/thread/FixedTest.java
+++ src/main/java/com/ocp/thread/FixedTest.java
@@ -1,4 +1,30 @@
 package com.ocp.thread;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 public class FixedTest {
+
+    public void test(){
+
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(() -> System.out.printf("THREAD NAME & Time: %s -- %s%n", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), Thread.currentThread().getName()), 1L, 7L, TimeUnit.SECONDS);
+        try {
+            boolean awaitTermination = service.awaitTermination(5L, TimeUnit.SECONDS);
+            if (!awaitTermination){
+                service.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        FixedTest test = new FixedTest();
+        test.test();
+    }
 }
